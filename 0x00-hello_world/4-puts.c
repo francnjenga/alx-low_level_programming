7457684c6c24db816ed3fa18037e96fcaed2fd4f
francnjenga <francnjenga9@gmail.com>
2022-12-10 22:19:46 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 {
-	puts("programming is like buiding a multillingual puzzle");
-	return 0;
+	puts("programming is like building a multilingual puzzle");
+	return (0);
 }
